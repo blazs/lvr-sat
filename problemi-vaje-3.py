26f46239c358a9e7a16e76a6abba979d32321d74
Blaz Sovdat <blaz.sovdat@gmail.com>
2014-03-07 13:29:09 +0100

    Added code that reduces graph k-coloring to SAT

---
+++ problemi-vaje-3.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Adapted Janos' code. :-) 
+#
+
+import prop
+import math
+import re
+
+# Združljivost za Python 2 in Python 3
+try:
+	basestring
+except NameError:
+	basestring = str
+	
+def iff(p, q):
+	"""Vrne logièno ekvivalenco izrazov p in q kot konjunkcijo dveh implikacij."""
+	return prop.And(prop.Implies(p, q), prop.Implies(q, p))
+
+####
+# Takes an instance of graph k-coloring problem (G,k) and outputs corresponsing Boolean formula Phi such
+# that Phi is satisfiable if and only if G is k-colorable. 
+####
+def graph_coloring(G, k):
+	# G[0] naj bo stevilo povezav 
+	# G[1] naj bo seznam parov vozlics, a.k.a, seznan pobexzav
+	# k > 0 je stevilo barv 
+	assert k>0, "Premalo barv"
+	
+	l = []
+	
+	# vsako vozlisce ima vsaj eno barvo 
+	l.append(prop.And([prop.Or(["v%dc%d" % (i, j) for j in range(k)]) for i in range(G[0])]))
+	# pari krajisc so razlicnih barv 
+	for (u, v) in G[1]:
+		l.append(prop.And([prop.And([prop.Not(prop.And(["v"+str(u)+"c"+str(c), "v"+str(v)+"c"+str(c)])) for c in range(k)])]))
+	# vsako vozlisce je kvecjemu ene barve 
+	for v in range(G[0]):
+		for i in range(k):
+			for j in range(i+1, k):
+				l.append(prop.Not(prop.And(["v"+str(v)+"c"+str(i), "v"+str(v)+"c"+str(j)])))
+	print prop.And(l)
+
+if __name__ == '__main__':
+	V = 3
+	E = [(0,1), (1,2)]
+	G = (V, E)
+	graph_coloring(G, 2)
+	
