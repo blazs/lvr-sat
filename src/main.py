9a806a29cf73e6f536c520224e7b8f691efed233
Blaz Sovdat <blaz.sovdat@gmail.com>
2014-03-28 11:53:52 +0100

    Dodal nekaj primerov uporabe

--- src/main.py
+++ src/main.py
@@ -4,17 +4,21 @@ import math
 import re
 import itertools
 import prop
-import freser_sat as mf
+import sat as sat
 import prevedbe as p
 import generate_tests as gen
 
 # Primer uporabe DPLL SAT solverja 
-def dpll_sat_test():
-	pass
+def dpll_sat_test(phi):
+	print "#"*3, "Resujem zadovoljivost z DPLL algoritmom", "#"*3
+	print "phi = ", phi
+	print sat.sat(phi)
 
 # Primer uporabe bruteforce SAT solverja 
-def bf_sat_test():
-	pass
+def bf_sat_test(phi):
+	print "#"*3, "Resujem zadovoljivost z bruteforce algoritmom", "#"*3
+	print "phi = ", phi
+	print sat.satBruteForce(phi)
 
 # Primer uporabe funkcije za generiranje SAT instanc 
 def generate_test():
@@ -22,8 +26,27 @@ def generate_test():
 
 # Primer uporabe funkcij za racunanje prevedb odlocitvenih problemov na SAT 
 def prevedbe_test():
-	pass
+	print "#"*3, "Prevajam k-barvanje grafa na SAT", "#"*3
+	G = (3, [(0,1), (1,2), (0,2)])
+	k = 2
+	print "Stevilo barv k = ", k , ", graf G = ", G
+	phi_g = p.graph_coloring(G, k)
+	print "Pripadajoc SAT phi = ", phi_g
+	return phi_g
 
 # Vstopna tocka 
 if __name__ == "__main__":
-	print mf.sat(p.graph_coloring([3, [(0,1), (0,2), (1,2)]], 2).cnf())
+	# Test prevedb 
+	print "*" * 80
+	print "Testiram prevedbe"
+	phi_g = prevedbe_test()
+	# Test DPLL SAT solverja 
+	print "*" * 80
+	print "Testiram DPLL SAT solver"
+	print dpll_sat_test(phi_g)
+	# Test bruteforce SAT solverja 
+	print "*" * 80
+	print "Testiram bruteforce SAT solver"
+	print bf_sat_test(phi_g)
+	
+	# print mf.sat(p.graph_coloring([3, [(0,1), (0,2), (1,2)]], 2).cnf())
