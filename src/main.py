8bd967c27ef0589c707c8bfeb9706eb61c1823b3
Martin Frešer <martin.freser@gmail.com>
2014-04-25 11:16:47 +0200

    Testiranje primerov

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import itertools
 import prop
 import sat as sat
 import prevedbe as p
+import sudoku
 import generate_tests as gen
 
 # Primer uporabe DPLL SAT solverja 
@@ -20,12 +21,18 @@ def bf_sat_test(phi):
 	print "phi = ", phi
 	print sat.satBruteForce(phi)
 
+# Primer uporabe DPLL SAT solverja z hevristiko
+def dpll_sat_heuristics_test(phi):
+	print "#"*3, "Resujem zadovoljivost z bruteforce algoritmom", "#"*3
+	print "phi = ", phi
+	print sat.sat2(phi)
+
 # Primer uporabe funkcije za generiranje SAT instanc 
 def generate_test():
 	pass
 
 # Primer uporabe funkcij za racunanje prevedb odlocitvenih problemov na SAT 
-def prevedbe_test():
+def obarljivostGrafa_test1():
 	print "#"*3, "Prevajam k-barvanje grafa na SAT", "#"*3
 	G = (3, [(0,1), (1,2), (0,2)])
 	k = 2
@@ -34,19 +41,35 @@ def prevedbe_test():
 	print "Pripadajoc SAT phi = ", phi_g
 	return phi_g.cnf()
 
-# Vstopna tocka 
+def testirajNaVsehSolverjih(phi):
+    # Test DPLL SAT solverja
+    print "*" * 80
+    print "Testiram DPLL SAT solver"
+    print dpll_sat_test(phi)
+    # Test bruteforce SAT solverja
+    print "*" * 80
+    print "Testiram bruteforce SAT solver"
+    print bf_sat_test(phi)
+    #Test DPLL SAT solverja s preprosto hevristiko
+    print "*" * 80
+    print "Testira DPLL SAT z hevristiko"
+    print dpll_sat_heuristics_test(phi)
+
+# Vstopna tocka
 if __name__ == "__main__":
-	# Test prevedb 
-	print "*" * 80
-	print "Testiram prevedbe"
-	phi_g = prevedbe_test()
-	# Test DPLL SAT solverja 
-	print "*" * 80
-	print "Testiram DPLL SAT solver"
-	print dpll_sat_test(phi_g)
-	# Test bruteforce SAT solverja 
-	print "*" * 80
-	print "Testiram bruteforce SAT solver"
-	print bf_sat_test(phi_g)
-	
-	# print mf.sat(p.graph_coloring([3, [(0,1), (0,2), (1,2)]], 2).cnf())
+	# Test prevedb
+
+    # 1. Obarljivost grafa
+    print "*" * 80
+    print "Testiram prevedbo na obarljivost grafa"
+    phi_g = obarljivostGrafa_test1()
+    testirajNaVsehSolverjih(phi_g)
+    print "#" * 80
+    #2. Sudokuprint "*" * 80
+    print "Testiram prevedbe"
+    phi_g = obarljivostGrafa_test1()
+    testirajNaVsehSolverjih(phi_g)
+    print "#" * 80
+
+
+
